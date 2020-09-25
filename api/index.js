f5d3da4207cba98c8ecc3c904521c5317016fbc6
Rajat Sharma <rajat.prototype@gmail.com>
2020-09-25 16:41:13 +0530

    Added email subscription api

---
+++ api/index.js
@@ -0,0 +1,48 @@
+const KoaRouter = require('koa-router');
+const json = require('koa-json');
+const api = new KoaRouter();
+const SubscriberRouter = require('./subscribe');
+
+api.use(json());
+api.use(
+	'/subscribe', 
+	SubscriberRouter.routes(), 
+	SubscriberRouter.allowedMethods()
+); // Use subscriber route
+
+/**
+ * Testing route
+ * @route GET /
+ */
+api.get('/test', async ctx => {
+	ctx.body = {
+		message: 'It\'s work'
+	}
+});
+
+/**
+ * Team route
+ * @route GET /team/[role?]
+ */
+api.get('/team/:role?', async ctx => {
+	const role = ctx.params.role || '';
+	const team = require('../values/team.json');
+	let response;
+
+	if (role && role.length > 0) {
+		response = Reflect.get(team, role) || null;
+	} 
+	else {
+		response = team;
+	}
+
+	if (!response) {
+		ctx.response.status = 404;
+	}
+
+	console.log(response);
+
+	return ctx.body = response || {};
+})
+
+module.exports = api;
---
+++ api/subscribe.js
@@ -0,0 +1,90 @@
+const KoaRouter = require('koa-router');
+const router = new KoaRouter();
+const json = require('koa-json');
+const Subscriber = require('../models/Subscriber');
+
+router.use(json());
+
+/**
+ * New email subscription route
+ * @route POST /
+ */
+router.post('/', async ctx => {
+	const { email } = ctx.request.body;
+	const userAgent = ctx.request.header['user-agent'] || undefined;
+	let payload = {};
+
+	// Undefined email
+	if (!email) {
+		ctx.response.status = 400; // Status 400
+		return ctx.body = {
+			message: "Require email address"
+		};
+	}
+
+	// Check if user already subscribed
+	if (await Subscriber.exists({ email })) {
+		ctx.response.status = 200; // Status 200
+		return ctx.body = {
+			message: "Your email is already subscribed"
+		};
+	}
+
+	// Create db payload
+	payload = {
+		email,
+		userAgent
+	}
+
+	// New subscriber
+	const newSubscriber = new Subscriber(payload);
+
+	try {
+		await newSubscriber.save(); 
+	} catch (err) {
+		ctx.response.status = 500; // Error
+		return ctx.body = {
+			message: "Error while email subscription process"
+		};
+	}
+
+	ctx.response.status = 201; // Status 201
+	return ctx.body = {
+		message: "Your email subscribed"
+	};
+});
+
+/**
+ * Remove email subscription route
+ * @route DELETE /[email]
+ */
+router.delete('/:email', async ctx => {
+	const { email } = ctx.params;
+
+	// User existence
+	if (await Subscriber.exists({ email })) {
+		await Subscriber.deleteOne({ email }, (err) => {
+			if (err) {
+				ctx.response.status = 500; // Status 500
+				return ctx.body = {
+					message: "Error while email email unsubscription process"
+				};
+			}
+
+			ctx.response.status = 200; // Status 200
+			return ctx.body = {
+				message: "You're email unsubscribed"
+			};
+		})
+	} 
+	else {
+		// Already unsubscribed
+		ctx.response.status = 400; // Status 400
+		return ctx.body = {
+			message: "Your email is already unsubscribed"
+		};
+	}
+
+})
+
+module.exports = router;
