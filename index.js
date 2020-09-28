f75fb15fb27a45028be98a70d14735fb15fc7e68
Rajat Sharma <rajat.prototype@gmail.com>
2020-09-28 19:39:02 +0530

    Improve log messages, template caching

--- index.js
+++ index.js
@@ -1,64 +1,56 @@
 "use strict"
+const onProduction = process.env.NODE_ENV == 'production';
+
 /**
  * Load env variables for local development
  * @local
  */
-if (process.env.NODE_ENV !== 'production') {
+if (!onProduction) {
    require('dotenv').config();
 }
 
-// Load
-const path = require('path');
-const fs = require('fs');
+// Imports
 const Koa = require('koa');
 const serve = require('koa-static');
 const render = require('koa-ejs');
-const bodyParser = require('koa-bodyparser');
-const router = require('./routes'); // Koa router
+const path = require('path');
+
+ // Koa router
+const router = require('./routes');
 
 // Initiate
 const app = new Koa(); // Koa instance
 
 const PORT = process.env.PORT || 4000; // Server PORT
+let cacheTemplate = false;
 
-app.use(bodyParser());
+// Set values according to platform
+if (onProduction) {
+   cacheTemplate = true;
+}
 
-// Setups
+// Base router
 app
-.use(router.routes())
-.use(router.allowedMethods()); // Router
+   .use(router.routes())
+   .use(router.allowedMethods()); // Router
 
-app.use(serve(path.join(__dirname, 'static'))); // Config static assets
+// Config static assets
+app.use(serve(path.join(__dirname, 'static')));
 
-// *** Resolve static javascripts ***
-/** @route GET /app.js */
-router.get('/app.js', async ctx => {
-   let content;
-   try {
-      content = fs.readFileSync(path.join(__dirname, 'static/app.js'), 'utf8');
-   } catch (err) {
-      ctx.status = 404;
-      return ctx.body = "Can't get /app.js";
-   }
-
-   ctx.res.setHeader('Content-Type', 'application/javascript');
-
-   return ctx.body = content;
-});
-
+// Render template
 render(app, {  // EJS Template rendering
    root: path.join(__dirname, 'views'),
    viewExt: 'ejs',
    layout: 'layout',
-   cache: false,
+   cache: cacheTemplate,
    debug: false
 })
 
 // Start listening
-app.listen(PORT, () => {
+app.listen(PORT, function () {
 
    /** @local */
-   if (process.env.NODE_ENV !== 'production') {
-      console.log(`visit http://localhost:${PORT}`);
+   if (!onProduction) {
+      console.log(`Server running at ${'\x1b[36m'}http://localhost:${PORT}${'\x1b[0m'}`);
    }
 });
